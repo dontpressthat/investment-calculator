45d837ad2be6df745ed82b4e4685ecdcebc89b64
dontpressthat <chris@chrisrau.ch>
2020-07-06 12:57:20 -0700

    Set up base files

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Investment Property Calculator</h1>
+    </React.Fragment>
+  )
+}
+
+export default App; 
